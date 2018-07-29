a697bc6d0b8480fbc78d0e7707844827992db980
C-Lion <esstepansky@gmail.com>
2018-07-29 20:00:07 +0300

    create Hello Word in C using Xcode

---
+++ xcode-C-files/C-HelloWorld/C-HelloWorld/main.c
@@ -0,0 +1,29 @@
+//Default Xcode C file code:
+//  main.c
+//  C-HelloWorld
+//
+//  Created by Admin on 7/29/18.
+//  Copyright © 2018 LTCHHW. All rights reserved.
+//
+
+//#include <stdio.h>
+
+//int main(int argc, const char * argv[]) {
+    // insert code here...
+//    printf("Hello, World!\n");
+//    return 0;
+//}
+//************************************************
+//************************************************
+//************************************************
+//https://www.youtube.com/watch?v=34shb-jBX9w
+//Xcode C Tutorial 1 HELLO WORLD
+//July 2018
+#include "stdio.h"
+
+int main(void)
+{
+    printf("Hello World!\n");
+    printf("C-Lion is here!\n");
+}
+
